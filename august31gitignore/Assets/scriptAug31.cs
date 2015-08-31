a19056c6bd76ff64fda68ab3bd12723a6a9575a4
Dawn Harper <Dawn.Harper@uvu.edu>
2015-08-31 15:38:03 -0600

    assignment 2 variables and functions

---
+++ august31gitignore/Assets/scriptAug31.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scriptAug31 : MonoBehaviour {
+
+	int myInt = 5;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (myInt);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
