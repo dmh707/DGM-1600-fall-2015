d4aaecaac9b2c83e28f3e4bdca5d0a59a7068f4e
Dawn Harper <Dawn.Harper@uvu.edu>
2015-08-31 15:49:51 -0600

    assignment2 again

--- august31gitignore/Assets/scriptAug31.cs
+++ august31gitignore/Assets/scriptAug31.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class scriptAug31 : MonoBehaviour {
 
-	int myInt = 5;
+	int myInt = 26;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log (myInt);
+		Debug.Log (MultiplyByTwo(myInt));
 	}
-	
+	int MultiplyByTwo(int number) {
+		int ret = number * 2;
+		return ret;
+	}
 	// Update is called once per frame
 	void Update () {
 	
