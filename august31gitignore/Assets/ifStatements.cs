5e3867745245d52794d21ed064eafce71f4e5a29
Dawn Harper <Dawn.Harper@uvu.edu>
2015-09-28 14:15:22 -0600

    dev2

---
+++ august31gitignore/Assets/ifStatements.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int first = 5;
+		int second = 6;
+		if (first < second && first == 6) {
+			Debug.Log ("works");
+		} else {
+			Debug.Log ("this shouldn't happen");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
