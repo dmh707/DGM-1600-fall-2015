f3624c5e84c772b03d56f7a3463a3ff1de023eb8
Dawn Harper <Dawn.Harper@uvu.edu>
2015-10-12 15:45:25 -0600

    scope and access variables

---
+++ august31gitignore/Assets/AnotherClass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass
+{
+	public int apples = 5;
+	public int bananas = 8;
+	public int strawberries = 2;
+	
+	
+	private int stapler;
+	private int sellotape;
+	
+	
+	public void FruitMachine (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Fruit total: " + answer);
+	}
+	
+	
+	private void OfficeSort (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Office Supplies total: " + answer);
+	}
+}
---
+++ august31gitignore/Assets/ScopeAndAccessModifiers.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAndAccessModifiers : MonoBehaviour
+{
+	public int alpha = 5;
+	public int alphabeta;
+	
+	private int beta = 0;
+	private int gamma = 5;
+	
+	
+	private AnotherClass myOtherClass;
+	
+	
+	void Start ()
+	{
+		alpha = 29;
+		alphabeta = alpha;
+		myOtherClass = new AnotherClass();
+		myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+
+
+	}
+	
+	
+	void Example (int pens, int crayons)
+	{
+		int answer;
+		answer = pens * crayons * alpha;
+		Debug.Log(answer);
+	}
+	
+	
+	void Update ()
+	{
+		if (alphabeta != alpha) {
+			Debug.Log ("Alpha is set to: " + alpha);
+			alphabeta = alpha;
+		}
+	}
+}
