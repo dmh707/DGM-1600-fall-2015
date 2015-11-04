303d87f6d6ba98fdb1faba6d5683ba5edfe8d402
Dawn Harper <Dawn.Harper@uvu.edu>
2015-11-04 15:06:07 -0700

    enums and before

---
+++ august31gitignore/Assets/enumerationss.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enumerationss : MonoBehaviour {
+	public enum browsers {firefox, chrome, IE};
+	public browsers myBrowser = browsers.chrome;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myBrowser = changeBrowser (myBrowser);
+	}
+	browsers changeBrowser (browsers def) {
+		if (def == browsers.chrome || def == browsers.firefox) {
+			def = browsers.IE;
+			return def;
+		} else {
+			def = browsers.chrome;
+			return def;
+		}
+	}
+}
---
+++ august31gitignore/Assets/switches.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switches : MonoBehaviour {
+	int happiness = 0;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (happiness);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	switch (happiness) {
+		case 0:
+			Debug.Log (happiness);
+			happiness += 20;
+			break;
+		case 20:
+			Debug.Log (happiness);
+			happiness += 20;
+			break;
+		case 40:
+			Debug.Log (happiness);
+			happiness += 20;
+			break;
+		case 60:
+			Debug.Log (happiness);
+			happiness += 20;
+			break;
+		case 80:
+			Debug.Log (happiness);
+			happiness += 20;
+			break;
+		case 100:
+			Debug.Log (happiness);
+			happiness += 20;
+			break;
+
+
+		}
+
+	}
+}
