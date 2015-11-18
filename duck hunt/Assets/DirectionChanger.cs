291d0b62c123b342f7081e655de310325928d4fd
Dawn Harper <Dawn.Harper@uvu.edu>
2015-11-18 15:39:32 -0700

    direction changer

---
+++ duck hunt/Assets/DirectionChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger : MonoBehaviour {
+	public enum Changer { Horizontal, Vertical}
+	public Changer changer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision hit)
+	{
+		if (hit.transform.tag == "Duck") {
+
+
+		}
+
+	}
+}
---
+++ duck hunt/Assets/DuckMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckMovement : MonoBehaviour {
+	public float speed = .2f;
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		DirectionChanger ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += (direction * speed);
+	}
+
+	public void RandomDirection() {
+		direction = new Vector3 (Random.Range (-1f, 1f), Random.Range (.2f, 1f), 0);
+	}
+	public void DirectionChanger(Vector3 _dir){
+		direction = new Vector3(direction.x * _dir.x, direction.y * _dir.y);
+	}
+}
