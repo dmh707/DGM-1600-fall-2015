0c48bd4e41debcae780d42863be283dbd366529d
Dawn Harper <Dawn.Harper@uvu.edu>
2015-11-18 14:54:50 -0700

    my duck spawn

---
+++ august31gitignore/duck hunt/Assets/DuckHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider hit) {
+		if (hit.tag == "Kill Zone") {
+			Destroy(this.gameObject);
+		}
+
+	}
+}
